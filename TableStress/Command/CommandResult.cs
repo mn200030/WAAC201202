01e3d94dd5235ea97de2b25f83ba52ed4c3f534c
Takekazu Omi <takekazuomi@gmail.com>
2012-12-02 20:02:39 +0900

    漏れのファイルを追加

---
+++ TableStress/Command/CommandResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableStress.Command
+{
+    public class CommandResult
+    {
+        public long Start {get;set;}
+        public long Elapsed { get; set; }
+        public object Data {get;set;}
+    }
+}
